b491bbf3c48b052a10e71b8f1d61e93bff78ab37
gaoyao <gaoyaomail@gmail.com>
2020-09-19 19:17:17 +0800

    加入常用hash算法函数

---
+++ Hash table search/Project/hash.h
@@ -0,0 +1,23 @@
+// SDBMHash算法
+unsigned int SDBMHash(char* str);
+
+// RS Hash算法
+unsigned int RSHash(char* str);
+
+// JS Hash算法
+unsigned int JSHash(char* str);
+
+// P.J.Weinberger Hash算法
+unsigned int PJWHash(char* str);
+
+// ELF Hash算法
+unsigned int ELFHash(char* str);
+
+// BKDR Hash算法
+unsigned int BKDRHash(char* str);
+
+// DJB Hash算法
+unsigned int DJBHash(char* str);
+
+// AP Hash算法
+unsigned int APHash(char* str);
