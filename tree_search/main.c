dbf760544ea7df2626c96b95929d089720bcc2a9
gaoyao <gaoyaomail@gmail.com>
2020-10-07 18:34:45 +0800

    初始化实验二相关文件并修复文件读写插件bug

--- tree_search/main.c
+++ tree_search/main.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main(){
-    printf("hell0.");
+#include "string_file_reader.h"
+
+int main()
+{
+    char *str = NULL;
+    FileHandler *file_a;
+    FileHandler *file_b;
+    file_a = open_file("dict.txt", "rb");
+    file_b = open_file("output.txt", "wb");
+    while (read_line(file_a, &str))
+    {
+        write_line(file_b, str);
+    }
+    close_file(file_a);
+    close_file(file_b);
+    system("pause");
     return 0;
 }
