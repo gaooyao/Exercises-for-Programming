ad9da4e36a3d11f6f363f0363f7a52565d8515db
Jahn1998 <1713084222@qq.com>
2020-10-08 22:33:10 +0800

    rawtrie-bug

--- tree_search/search_rawtrie.c
+++ tree_search/search_rawtrie.c
@@ -1,19 +1,68 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "search_rawtrie.h"
+
+
+TrieNode *root;//root
 void rawtrie_init_tree()
 {
+    root = (TrieNode *)malloc(sizeof(TrieNode));
+  
+    root->count = 0;
+    int i;
+    for (i = 0; i < 256;i++)
+    {
+        root->child[i] = NULL;
+    }
+    printf("init w\n");
+}
+TrieNode* rawtrie_new_tree(){
+    TrieNode * obj = (TrieNode *)malloc(sizeof(TrieNode));
+    obj->count = 0;
+    int i;
+    for (i = 0; i < 256;i++)
+    {
+        obj->child[i] = NULL;
+    }
     printf("init w\n");
 }
 
-int rawtrie_insert_recoder(char *str)
-{
-    return 1;
+int rawtrie_insert_recoder(char *str){
+    char *p = str;
+    TrieNode *node = root;
+    while (*p) {
+        if (node->child[*p] == NULL) {
+            /* 对应的节点不存在，则申请一个节点 */
+            node->child[(int)(*p)] = rawtrie_new_tree();
+        }
+        node = node->child[*p]; /* 指向子节点 */
+        p++;
+    }
+    node->count++;
 }
 
 int rawtrie_query_recoder(char *str)
 {
-    return 1;
+    TrieNode *node = root;//指针指向root
+    char *p = str;
+
+    while (*p && node != NULL) {
+        node = node->child[*p]; /* 循环在树中查找所有节点 */
+        p++;
+    }
+
+    if (node == NULL) {//str未结束，而node已经不存在，则返回未找到
+        return 0; /* 未找到 */
+    } 
+    else {
+        return (node->count > 0 ? (1) : (0)); /*  */
+        printf("\n");
+    }
 }
 void rawtrie_destroy_tree()
 {
+    if (root != NULL) {
+        free(root);
+    }
     printf("destroy w\n");
 }
--- tree_search/search_rawtrie.h
+++ tree_search/search_rawtrie.h
@@ -1,6 +1,11 @@
 #ifndef search_rawtrie
 #define search_rawtrie
-
+typedef struct TrieNode {
+    struct TrieNode *child [256];
+    int isEndOfWord;
+    int count;  //子节点的数量
+    char value;//该节点的字符
+} TrieNode;
 void rawtrie_init_tree();
 int rawtrie_insert_recoder(char *str);
 int rawtrie_query_recoder(char *str);
