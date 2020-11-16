3c0b1f392c1c560ad067c6353247445f9f80dd99
gaooyao <gaoyaomail@gmail.com>
2020-11-16 18:57:24 +0800

    优化pagerank算法

--- network_vip_node/main.c
+++ network_vip_node/main.c
@@ -8,14 +8,14 @@
 int main()
 {
     clock_t s, e;
-    s = clock();
+    //s = clock();
     //build_graph(); //输出出链信息到graph.bin
-    e = clock();
-    //printf("time %f s.", (float)(e - s) / CLOCKS_PER_SEC);
+    //e = clock();
+    //printf("Build graph.bin time %f s.", (float)(e - s) / CLOCKS_PER_SEC);
 
     s = clock();
-    page_rank(); //输出出链信息到graph.bin
+    page_rank_v2(); //输出出链信息到graph.bin
     e = clock();
-    printf("ALL time: %f s.", (float)(e - s) / CLOCKS_PER_SEC);
+    printf("ALL time: %f s.\n", (float)(e - s) / CLOCKS_PER_SEC);
     return 0;
 }
