63848bfec62ca7336c5d3b179501ec168c08a811
gaooyao <gaoyaomail@gmail.com>
2020-11-21 00:04:46 +0800

    优化bin生成

---
+++ network_vip_node/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <sys/time.h>
+
+#include "build_graph.h"
+#include "page_rank.h"
+
+int main()
+{
+    struct timeval s, e;
+    gettimeofday(&s, NULL);
+
+    build_graph(); //输出出链信息到graph.bin
+    gettimeofday(&e, NULL);
+    printf("ALL time: %.3f s.\n", (e.tv_sec - s.tv_sec) + (e.tv_usec - s.tv_usec) / 1000000.0);
+    return 0;
+}
